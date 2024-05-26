7599c1176a5ba48678f170f3f4fe5b24b49e303b
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-05-26 08:58:16 -0700

    Checking

--- models/user.py
+++ models/user.py
@@ -14,14 +14,14 @@ class User(BaseModel, Base):
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
-        password = Column("password", String(128), nullable=False)
+        _password = Column("password", String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user")
         reviews = relationship("Review", backref="user")
     else:
         email = ""
-        password = ""
+        _password = ""
         first_name = ""
         last_name = ""
 
@@ -34,9 +34,9 @@ class User(BaseModel, Base):
     @property
     def get_pwd(self):
         # password(Getter)
-        return (self.password)
+        return (self._password)
 
     @password.setter
     def set_password(self, value):
         # Passwords setter + hashing
-        self.password = hashlib.md5(value.encode()).hexdigest()
+        self._password = hashlib.md5(value.encode()).hexdigest()
