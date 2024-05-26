84dc4d143f0ee74d3a3daa5dfd917e2ee0f17032
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-05-26 08:59:45 -0700

    Rolled back

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ holds class User"""
 import models
-import hashlib
+# import hashlib
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
@@ -14,29 +14,17 @@ class User(BaseModel, Base):
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
-        _password = Column("password", String(128), nullable=False)
+        password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user")
         reviews = relationship("Review", backref="user")
     else:
         email = ""
-        _password = ""
+        password = ""
         first_name = ""
         last_name = ""
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
-        if "password" in kwargs:
-            self.password = kwargs["password"]
-
-    @property
-    def get_pwd(self):
-        # password(Getter)
-        return (self._password)
-
-    @password.setter
-    def set_password(self, value):
-        # Passwords setter + hashing
-        self._password = hashlib.md5(value.encode()).hexdigest()
